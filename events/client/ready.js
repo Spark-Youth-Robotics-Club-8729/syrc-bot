110032ec4117d2d57340ccc2e14097a6e694c764
FinityFly <daniel1211.lu@gmail.com>
2022-07-12 15:13:39 -0400

    Added presence
    don't click on the link

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,12 @@
-module.exports = () => {
+module.exports = (Discord, client) => {
     console.log('Bot is online!');
+    client.user.setPresence({
+        status: "online",
+        activities: [{
+            name: "!help | SYRC",
+            type: "STREAMING",
+            url: "https://www.youtube.com/watch?v=8SIiGo3TVKE",
+            details: "sparky :)"
+        }]
+    });
 }
