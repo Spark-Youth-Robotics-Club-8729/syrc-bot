ee311eb64f16aa5be7bdd2ca90238780a819a610
FinityFly <daniel1211.lu@gmail.com>
2022-07-12 16:27:20 -0400

    Update weather.js
    Bug fixes

--- commands/weather.js
+++ commands/weather.js
@@ -7,30 +7,27 @@ module.exports = {
     async execute(client, message, args, Discord) {
         const weather_api_key = "046bc80be1c3e7a199f6995c46b6c94b";
         const loc = args[0];
-        let getWeather = async () => {
+        try {
             let res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${weather_api_key}`);
-            return res.data;
-        }
-        let weatherData = await getWeather();
-        const newEmbed = {};
-        if (weatherData["cod"] == 200) {
-            newEmbed = {
+            let weatherData = res.data;
+            let newEmbed = {
                 color: '#5F75DE',
                 title: "Weather of " + loc.charAt(0).toUpperCase() + loc.slice(1),
                 description: weatherData["weather"][0]["main"],
                 fields: [
-                    { name: "Temperature", value: (weatherData["main"]["temp"] - 273.15).toString(), inline: true },
-                    { name: "Feels Like", value: (weatherData["main"]["feels_like"] - 273.15).toString(), inline: true },
+                    { name: "Temperature", value: (Math.round((weatherData["main"]["temp"] - 273.15)*10)/10).toString() + "°C", inline: true },
+                    { name: "Feels Like", value: (Math.round((weatherData["main"]["feels_like"] - 273.15)*10)/10).toString() + "°C", inline: true },
                     { name: "Humidity", value: weatherData["main"]["humidity"].toString() + "%", inline: true },
-                    { name: "Wind Speed", value: weatherData["wind"]["speed"].toString() + "m/s", inline: true },
+                    { name: "Wind Speed", value: weatherData["wind"]["speed"].toString() + " m/s", inline: true },
                 ]
             }
-        } else {
+            message.channel.send({ embeds: [newEmbed] });
+        } catch {
             const newEmbed = {
                 color: '#5F75DE',
                 description: "City not found :("
             }
+            message.channel.send({ embeds: [newEmbed] });
         }
-        message.channel.send({ embeds: [newEmbed] });
     }
 } 
