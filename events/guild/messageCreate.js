d84f1b6c515f8e3191cda5318674adb703fc8ed1
FinityFly <daniel1211.lu@gmail.com>
2022-07-13 10:49:29 -0400

    Added dadbot!

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -1,7 +1,13 @@
-module.exports = (Discord, client, message) => {
+module.exports = async (Discord, client, message) => {
+    if (message.content.startsWith("im ") || message.content.startsWith("Im ")) {
+        await message.channel.send(`Hi ${message.content.substring(3)}, I'm dad!`);
+    } else if (message.content.startsWith("i'm ") || message.content.startsWith("I'm ")) {
+        await message.channel.send(`Hi ${message.content.substring(4)}, I'm dad!`);
+    }
     const prefix = '!';
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     if (message.channel.id != 984570241193496646 && message.channel.id != 988138295869468743) return;
+    
     const args = message.content.slice(prefix.length).split(/ +/);
     const cmd = args.shift().toLowerCase();
 
