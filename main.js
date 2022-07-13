45dd5e35332f2040ddf19eac14b347940dce55fc
FinityFly <daniel1211.lu@gmail.com>
2022-07-13 11:08:45 -0400

    Trying to fix heroku porting issues

--- main.js
+++ main.js
@@ -4,6 +4,19 @@ const { Client, Intents } = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"], intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
 const mysql = require(`mysql`);
 
+// to fix heroku porting issues
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 // const syrcdb = mysql.createConnection({
 //     host: 'localhost',
 //     user: 'root',
