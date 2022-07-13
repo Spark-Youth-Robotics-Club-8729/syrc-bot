d43435f87eca1c812088b5ce5e42686d1825f195
FinityFly <daniel1211.lu@gmail.com>
2022-07-13 13:44:59 -0400

    Migrated to mysql2

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const config = require('./config.json')
 const { Client, Intents } = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"], intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
-const mysql = require(`mysql`);
+const mysql2 = require(`mysql2`);
 
 // to fix heroku porting issues
 var express = require('express');
