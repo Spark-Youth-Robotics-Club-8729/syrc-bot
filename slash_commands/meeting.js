e070f84cc997155b1f42a73e8513d702103c8c3b
Iwillgetmy <74886842+Iwillgetmy@users.noreply.github.com>
2022-07-26 11:13:52 -0400

    Update meeting.js

--- slash_commands/meeting.js
+++ slash_commands/meeting.js
@@ -35,46 +35,51 @@ module.exports = {
                 .setRequired(false)
         ),
     run: async (client, interaction, args, message) => {
-        const datetime = interaction.options.getString("datetime");
-        const subteams = interaction.options.getRole("subteam");
-        var notes = interaction.options.getString("notes");
-        if (notes === null) {
-            notes = "N/A";
-        }
-        var date = new Date(datetime);
-        var curDate = new Date();
-        if (Number.isNaN(date.getTime()/1000)) {
-            return await interaction.reply('Incorrect date and time formatting');
-        } else if (curDate.getTime() > date.getTime()) {
-            return await interaction.reply("Time traveling isn't allowed sorry");
-        }
-        const newEmbed = new Discord.MessageEmbed()
-                    .setTitle(`${subteams.name} meeting`)
-                    .setColor("#5F75DE")
-                    .setDescription(`**Starting:** <t:${date.getTime() / 1000}:R>`)
-                    .addField( "Notes", `*${notes}*` )
-                    .setThumbnail('https://i.postimg.cc/dQjY2YNS/Screen-Shot-2022-03-07-at-9-00-41-PM.png')
-        let rawdata = fs.readFileSync('./config.json');
-        let config = JSON.parse(rawdata);
-        let channel = client.channels.cache.get(config.meetingchannel[0].channel_id);
-        let msg = await channel.send({ embeds: [newEmbed] });
-        let link = `https://discord.com/channels/${msg.guild.id}/${msg.channel.id}/${msg.id}`;
-        config.meetings.push({"start_time": (date.getTime() / 1000).toString(), "subteam_id": subteams.id, "notes": notes, "msg_link": link});
-        const configString = JSON.stringify(config);
-        fs.writeFile('./config.json', configString, err => {
-            if (err) {
-                console.log('Error fetching data', err);
-            } else {
-                console.log('Successfully fetched data!');
+        if (interaction.member.roles.cache.some(role => role.name === 'Lead')) {
+
+            const datetime = interaction.options.getString("datetime");
+            const subteams = interaction.options.getRole("subteam");
+            var notes = interaction.options.getString("notes");
+            if (notes === null) {
+                notes = "N/A";
             }
-        })
-        await pgClient.query(`INSERT INTO meetings (start_time, subteam_id, notes, msg_link) VALUES ('${date.getTime() / 1000}','${subteams.id}','${notes}', '${link}')`, async (err, res) => {
-            if (!err) {
-                await interaction.reply(`Meeting created`);
-            } else {
-                await interaction.reply(`Ewah`);
-                throw err;
+            var date = new Date(datetime);
+            var curDate = new Date();
+            if (Number.isNaN(date.getTime()/1000)) {
+                return await interaction.reply('Incorrect date and time formatting');
+            } else if (curDate.getTime() > date.getTime()) {
+                return await interaction.reply("Time traveling isn't allowed sorry");
             }
-        })
+            const newEmbed = new Discord.MessageEmbed()
+                        .setTitle(`${subteams.name} meeting`)
+                        .setColor("#5F75DE")
+                        .setDescription(`**Starting:** <t:${date.getTime() / 1000}:R>`)
+                        .addField( "Notes", `*${notes}*` )
+                        .setThumbnail('https://i.postimg.cc/dQjY2YNS/Screen-Shot-2022-03-07-at-9-00-41-PM.png')
+            let rawdata = fs.readFileSync('./config.json');
+            let config = JSON.parse(rawdata);
+            let channel = client.channels.cache.get(config.meetingchannel[0].channel_id);
+            let msg = await channel.send({ embeds: [newEmbed] });
+            let link = `https://discord.com/channels/${msg.guild.id}/${msg.channel.id}/${msg.id}`;
+            config.meetings.push({"start_time": (date.getTime() / 1000).toString(), "subteam_id": subteams.id, "notes": notes, "msg_link": link});
+            const configString = JSON.stringify(config);
+            fs.writeFile('./config.json', configString, err => {
+                if (err) {
+                    console.log('Error fetching data', err);
+                } else {
+                    console.log('Successfully fetched data!');
+                }
+            })
+            await pgClient.query(`INSERT INTO meetings (start_time, subteam_id, notes, msg_link) VALUES ('${date.getTime() / 1000}','${subteams.id}','${notes}', '${link}')`, async (err, res) => {
+                if (!err) {
+                    await interaction.reply(`Meeting created`);
+                } else {
+                    await interaction.reply(`Ewah`);
+                    throw err;
+                }
+            })
+        } else{
+            interaction.followUp("Insufficient Permissions");
+        }
     }
 }
