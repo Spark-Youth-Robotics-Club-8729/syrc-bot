5599c096fa1f026e7653f48538c6f06acbb48f90
Iwillgetmy <74886842+Iwillgetmy@users.noreply.github.com>
2022-07-26 11:11:56 -0400

    Update censor.js

--- slash_commands/censor.js
+++ slash_commands/censor.js
@@ -1,1 +1,22 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { CommandInteraction } = require("discord.js");
+const Discord = require("discord.js");
+const config = require('./../config.json');
 
+module.exports = {
+    ...new SlashCommandBuilder()
+        .setName("censor")
+        .setDescription("Adds words to the censor list")
+        .addStringOption(option => option.setName("word").setDescription("Word to add to censor list").setRequired(true)),
+    run: async (client, interaction, args) => {
+        if (interaction.member.roles.cache.some(role => role.name === 'God')) {
+
+            const word = interaction.options.getString("word");
+            config.censorList.push(word);
+            interaction.followUp("word added")
+        } else {
+            interaction.followUp("Insufficient permissions");
+        }
+    }
+
+}
