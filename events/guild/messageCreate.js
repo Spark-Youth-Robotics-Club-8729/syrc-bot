9782550bdca5870b37089c44e55e3bb6f0f70840
Iwillgetmy <74886842+Iwillgetmy@users.noreply.github.com>
2022-07-26 11:29:38 -0400

    Update messageCreate.js

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -13,16 +13,16 @@ module.exports = async (Discord, client, message) => {
         await message.channel.send(`Hi ${message.content.substring(4)}, I'm dad!`);
     }
 
-    var arr = config.censorList;
-    message.channel.send(arr[0]);
-    for (var i = 0; i < arr.length; i++) {
-        if (message.content.toLowerCase().includes(arr[i])){
-            console.log(arr[i]);
+
+    message.channel.send(config.censorList[0]);
+    for (var i = 0; i < config.censorList.length; i++) {
+        if (message.content.toLowerCase().includes(config.censorList[i])){
+            console.log(config.censorList[i]);
             member = message.author.id.toString();
             if (message.author.id != 880532935445454929) {
                 message.channel.bulkDelete(1);
             }
-            if (arr[i]=="owo" || arr[i] == "uwu") {
+            if (config.censorList[i]=="owo" || config.censorList[i] == "uwu") {
                 message.channel.send({ content: `<@${member}> ur a stinky furry :)`, tts: true });
                 break
             } else{
