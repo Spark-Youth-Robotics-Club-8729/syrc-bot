d1442511bf40c6239d9a0ef5cfeb779bc0c924f8
Finity <56236512+FinityFly@users.noreply.github.com>
2022-07-29 10:19:43 -0400

    Update debug.js

--- slash_commands/debug.js
+++ slash_commands/debug.js
@@ -31,7 +31,7 @@ module.exports = {
         let config = await fetch_data();
         console.log("RETURNED CONFIG");
         console.log(config);
-        config.remove('typinglb');
+        delete config['typinglb'];
         await interaction.reply({ content: `\`\`\`json\n${JSON.stringify(config, null, 2)}\`\`\``, ephemeral: true });
     }
 };
