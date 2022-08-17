333094265cfb297dc4e29d16530f371db7e7fff2
Iwillgetmy <74886842+Iwillgetmy@users.noreply.github.com>
2022-08-17 11:39:35 -0400

    Update messageCreate.js

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -7,19 +7,6 @@ module.exports = async (Discord, client, message) => {
         return;
     }   
 
-    if (message.content.toLowerCase().startsWith("im ")) {
-        if(parseInt(Math.random()*10)==2){
-            await message.channel.send(`Hi ${message.content.substring(3)}, I'm dad!`);
-        }
-    } else if (message.content.toLowerCase().startsWith("i'm ")) {
-        if(parseInt(Math.random()*10)==2){
-            await message.channel.send(`Hi ${message.content.substring(4)}, I'm dad!`);
-        }
-    } else if ( message.content.toLowerCase().startsWith("i am ")){
-        if(parseInt(Math.random()*10)==2){
-            await message.channel.send(`Hi ${message.content.substring(5)}, I'm dad!`);
-        }
-    }
     let filter = new Filter();
     let rawData = fs.readFileSync('./config.json');
     let config = JSON.parse(rawData);
@@ -46,15 +33,16 @@ module.exports = async (Discord, client, message) => {
     if(message.content.toLowerCase().includes('gp')){
         message.channel.send("Who are you, Tony?");
     }
-    if(parseInt(Math.random()*100)==2){
-        if(message.channel.id==853288359505821716 || message.channel.id==904848756090998834){
-            message.channel.send("no one asked?");
-        }
-    }
+
     if (message.channel.id == config.countingchannel[0].channel_id) { // this needs to be fetched from config.json later
         pgClient.query(`SELECT * FROM counting`, async (err, res) => {
             if (message.content.startsWith(parseInt(res.rows[0].number)+1) && message.author.id != res.rows[0].user_id) { // might not work idk if the [0] should be there :clown:
-                pgClient.query(`UPDATE counting SET number = ('${parseInt(res.rows[0].number)+1}'), user_id = ('${message.author.id}')`);
+                if(parseInt(Math.random()*10)==2){
+                    message.channel.send(parseInt(res.rows[0].number)+1);
+                    pgClient.query(`UPDATE counting SET number = ('${parseInt(res.rows[0].number)+1}'), user_id = ("880532935445454929");
+                }else{
+                    pgClient.query(`UPDATE counting SET number = ('${parseInt(res.rows[0].number)+1}'), user_id = ('${message.author.id}')`);
+                }
             } else {
                 await message.delete();
             }
