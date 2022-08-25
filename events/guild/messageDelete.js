b899ebf2b130ca1bf8b365e35a7068d4088b95d7
Iwillgetmy <74886842+Iwillgetmy@users.noreply.github.com>
2022-08-25 12:17:45 -0400

    Update messageDelete.js

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -20,15 +20,5 @@ module.exports = (Discord, client, message) => {
         .setTimestamp()
         .setFooter(client.user.tag, client.user.displayAvatarURL());
     client.channels.cache.get("988138295869468743").send({ embeds: [newEmbed] });
-    if (message.channel.id == config.countingchannel[0].channel_id) { // this needs to be fetched from config.json later
-        pgClient.query(`SELECT * FROM counting`, async (err, res) => {
 
-            if (message.content.startsWith(parseInt(res.rows[0].number))) { // might not work idk if the [0] should be there :clown:
-                message.chanel.send(`${res.rows[0].number}`);
-                pgClient.query(`UPDATE counting SET user_id = ('${message.author.id}')`);
-            } else {
-                await message.delete();
-            }
-        })
-    }
 }
