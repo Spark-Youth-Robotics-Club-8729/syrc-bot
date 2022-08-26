59fef14e706dea8d7f331594d98f9e33061af9e4
Iwillgetmy <74886842+Iwillgetmy@users.noreply.github.com>
2022-08-25 20:23:17 -0400

    Update messageCreate.js

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -58,7 +58,7 @@ module.exports = async (Discord, client, message) => {
     if (message.channel.id == config.countingchannel[0].channel_id) { // this needs to be fetched from config.json later
         pgClient.query(`SELECT * FROM counting`, async (err, res) => {
                 if (message.content.startsWith(parseInt(res.rows[0].number)+1) && message.author.id != res.rows[0].user_id) { // might not work idk if the [0] should be there :clown:
-                    if(parseInt(Math.random()*3)==2){
+                    if(parseInt(Math.random()*20)==2){
                         pgClient.query(`UPDATE counting SET number = ('${parseInt(res.rows[0].number)+2}'), user_id = ('${client.user.id}')`);
                         botMsg=true
                         await message.channel.send(`${parseInt(res.rows[0].number)+2}`);
