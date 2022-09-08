537b4689a5a40a38c574084335d138bb4d37891f
Iwillgetmy <74886842+Iwillgetmy@users.noreply.github.com>
2022-09-07 20:05:06 -0400

    Update messageCreate.js

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -52,9 +52,9 @@ module.exports = async (Discord, client, message) => {
         }
     }
     const prefix = '!';
-    if(message.content.toLowerCase().includes(' gp ') || message.content.toLowerCase().includes(' gp') || message.content.toLowerCase().includes('gp ')){
-        message.channel.send("Who are you, Tony?");
-    }
+    //if(message.content.toLowerCase().includes(' gp ') || message.content.toLowerCase().includes(' gp') || message.content.toLowerCase().includes('gp ')){
+    //    message.channel.send("Who are you, Tony?");
+    //}
     if (message.channel.id == config.countingchannel[0].channel_id) { // this needs to be fetched from config.json later
         pgClient.query(`SELECT * FROM counting`, async (err, res) => {
                 if (message.content.startsWith(parseInt(res.rows[0].number)+1) && message.author.id != res.rows[0].user_id) { // might not work idk if the [0] should be there :clown:
