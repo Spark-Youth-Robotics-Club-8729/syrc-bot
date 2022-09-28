80af25a490f140853aeabe37c11cf3cf5d0a93c3
Iwillgetmy <74886842+Iwillgetmy@users.noreply.github.com>
2022-09-27 21:37:09 -0400

    Update guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 
 module.exports = (Discord, client, member) => {
-    console.log("HI");
     
     let rawdata = fs.readFileSync('./config.json');
     let config = JSON.parse(rawdata);
@@ -9,6 +8,7 @@ module.exports = (Discord, client, member) => {
     const linkschannel = member.guild.channels.cache.get('862089529698811934');
     const message = `Welcome <@${member.id}> to FRC team 8729: Sparkling H2O! Check out ${rolechannel} to get roles and ${linkschannel} for useful links throughout your journey. Awesome to have you here :D`;
     const channel = member.guild.channels.cache.get(config.welcomechannel[0].channel_id);
-
+    const member = reaction.message.guild.members.cache.get(user.id);
+    await member.roles.add('929494438038880306');
     channel.send(message);
 }
