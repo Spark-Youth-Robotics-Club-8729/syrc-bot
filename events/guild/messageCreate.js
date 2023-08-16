016e0a963ab1269f416c130887f70652b9df938c
Finity <56236512+FinityFly@users.noreply.github.com>
2023-08-15 22:46:32 -0400

    Update messageCreate.js

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -55,7 +55,7 @@ module.exports = async (Discord, client, message) => {
     //if(message.content.toLowerCase().includes(' gp ') || message.content.toLowerCase().includes(' gp') || message.content.toLowerCase().includes('gp ')){
     //    message.channel.send("Who are you, Tony?");
     //}
-    if (message.channel.id == config.countingchannel[0].channel_id) { // this needs to be fetched from config.json later
+    try (message.channel.id == config.countingchannel[0].channel_id) { // this needs to be fetched from config.json later
         pgClient.query(`SELECT * FROM counting`, async (err, res) => {
                 if (message.content.startsWith(parseInt(res.rows[0].number)+1) && message.author.id != res.rows[0].user_id) { // might not work idk if the [0] should be there :clown:
                     if(parseInt(Math.random()*20)==2){
@@ -69,6 +69,8 @@ module.exports = async (Discord, client, message) => {
                     await message.delete();
                 }
         })
+    } catch (TypeError) {
+        console.log("No counting channel set... continuing");
     }
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     var allowed_channels = [];
