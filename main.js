403d0106af494a85633c48b67ee1f1ea5cadd032
vdoubleu <victorwang2001@gmail.com>
2023-08-15 22:39:11 -0400

    deploy to fly

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const client = new Discord.Client({
     intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_MEMBERS, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Discord.Intents.FLAGS.GUILD_VOICE_STATES, "GUILDS", "GUILD_VOICE_STATES"] 
 });
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8080));
 
 app.get('/', function(request, response) {
     var result = 'App is running'
