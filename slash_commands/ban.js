2d480b660d94478835f152602337e232970e0702
Finity <56236512+FinityFly@users.noreply.github.com>
2023-08-17 12:13:11 -0400

    Update ban.js

--- slash_commands/ban.js
+++ slash_commands/ban.js
@@ -19,29 +19,26 @@ module.exports = {
                 modroles.push(config.modrole[i].role_id);
             }
             if (interaction.member.roles.cache.some(role => modroles.includes(role.id))) {
-                if (member) {
-                    const memberTarget = interaction.guild.members.cache.get(member.id);
-                    let reason = args.slice(1).join(" ");
-                    if (!reason) reason = "No reason given";
-                    if (memberTarget.kickable) {
-                        const kickEmbed = new Discord.MessageEmbed()
-                            .setTitle(`You were banned from ${interaction.guild.name}`)
-                            .setDescription(`Reason: ${reason}`)
-                            .setColor("#5F75DE")
-                            .setTimestamp()
-                            .setFooter(client.user.tag, client.user.displayAvatarURL());
-                        try {
-                            await memberTarget.send({ embeds: [kickEmbed] });
-                        } catch (err) {
-                            // pass
-                        }
-                        memberTarget.ban();
-                        interaction.reply("User has been banned");
-                    } else {
-                        interaction.reply("Insufficient Permissions");
+                const user = interaction.options.getUser("user");
+                const memberTarget = interaction.guild.members.cache.get(user.id);
+                let reason = args.slice(1).join(" ");
+                if (!reason) reason = "No reason given";
+                if (memberTarget.kickable) {
+                    const kickEmbed = new Discord.MessageEmbed()
+                        .setTitle(`You were banned from ${interaction.guild.name}`)
+                        .setDescription(`Reason: ${reason}`)
+                        .setColor("#5F75DE")
+                        .setTimestamp()
+                        .setFooter(client.user.tag, client.user.displayAvatarURL());
+                    try {
+                        await memberTarget.send({ embeds: [kickEmbed] });
+                    } catch (err) {
+                        // pass
                     }
+                    memberTarget.ban();
+                    interaction.reply("User has been banned");
                 } else {
-                    interaction.reply('No member specified');
+                    interaction.reply("Insufficient Permissions");
                 }
             } else {
                 interaction.reply("Lol you don't have perms to ban people");
